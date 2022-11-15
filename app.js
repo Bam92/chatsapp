343790edc0d9a985a86eeabdace9fa1341433045
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2022-11-15 22:27:01 +0200

    feat: connect the frontend w/ the backend

--- app.js
+++ app.js
@@ -4,9 +4,15 @@ import morgan from 'morgan'
 
 const app = express()
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 app.use(morgan('tiny'))
 
-app.get('/', (req, res) => res.status(200).json({ status: 200, success: true, message: 'Welcome! The server is working properly' }));
+app.get('/', (req, res) => res.status(200).json({ "status": 200, "success": true, "message": 'Welcome! The server is working properly' }));
 
 // Error handler
 app.use((req, res) => {
