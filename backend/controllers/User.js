a307e2d65cc7f1b3941eaf5c51e94bf451ccb930
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2022-11-27 21:23:56 +0200

    feat: allow user to sign in
    - bcrypt.compare()
    - jwt to generate token

--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 import "dotenv/config.js";
 
 import User from "../Models/User.js";
@@ -16,6 +17,39 @@ router.post("/signup", async (req, res) => {
             "success": true, 
             "message": `User ${req.body.username} created successfully`
          })
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+router.post("/login", async (req, res) => {
+    try {
+        const username = req.body.username
+        const user = await User.findOne({ username, })
+
+        if (user) {
+            const result = await bcrypt.compare(req.body.password, user.password)
+            if (result) {
+                // sign token and send it in response
+                const token = await jwt.sign({ username: user.username, }, process.env.TOKEN_SECRET)
+                
+                res.header("auth-token", token).status(200).json({
+                    token,
+                    "message":"login successfully",
+                    "success": true,
+                    "status": 200
+                })
+
+            } else res.status(403).json({
+                error: "Password does not match. Try again",
+                "success": false,
+                "status": 403
+            })
+
+        } else res.status(404).json({
+            error: `User ${username} doesn't exist in the app`
+        })
+        
     } catch (error) {
         res.status(400).json(error)
     }
